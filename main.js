e71beaa3b01cbdb5d4660d5d04e5b1b587d18e53
NiklasSchw <schwardnik@yahoo.de>
2024-04-18 17:21:16 +0200

    Overlay für Sehenswürdigkeiten definiert

--- main.js
+++ main.js
@@ -14,6 +14,10 @@ let map = L.map("map").setView([stephansdom.lat, stephansdom.lng], 12);
 let startLayer = L.tileLayer.provider("BasemapAT.grau");
 startLayer.addTo(map);
 
+let themaLayer = {
+  sights: L.featureGroup().addTo(map),
+}
+
 // Hintergrundlayer
 L.control
   .layers({
@@ -25,6 +29,8 @@ L.control
     "BasemapAT Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
     "BasemapAT Beschriftung": L.tileLayer.provider("BasemapAT.overlay"),
     "Esri.WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
+  }, {
+    "Sehenswürdigkeiten": themaLayer.sights,
   })
   .addTo(map);
 
@@ -45,18 +51,11 @@ L.control
   .fullscreen()
   .addTo(map);
 
-// function addiere(zahl1, zahl2) {
-//   let summe = zahl1 + zahl2;
-//   console.log("Summe:", summe);
-// }
-
-// addiere(4, 7);
-
 async function loadSights(url) {
   //console.log("Loading", url);
   let response = await fetch(url);
   let geojson = await response.json();
   //console.log(geojson);
-  L.geoJSON(geojson).addTo(map);
+  L.geoJSON(geojson).addTo(themaLayer.sights);
 }
 loadSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json")
